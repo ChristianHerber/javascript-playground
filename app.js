5e6afceefbace41d49531d4d8230670a6afd895d
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-07-01 13:36:49 -0400

    Exercise: Introduction to javascript

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.onload = () => {
     console.log('This is your first program executing!');
 }
+
+const our_name = document.querySelector(".b")
+our_name.addEventListener('click', setName)
+function setName(){
+    const n = prompt("Write your name")
+    our_name.innerHTML = `Your name is ${n}`
+}
